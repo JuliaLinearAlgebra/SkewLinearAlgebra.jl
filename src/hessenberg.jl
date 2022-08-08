b9ab6ef5498d0c339245fa1aa5dedd0b78d8a482
smataigne <simonmataigne@gmail.com>
2022-08-08 22:59:00 +0200

    complex hess

--- src/hessenberg.jl
+++ src/hessenberg.jl
@@ -260,3 +260,95 @@ end
 
 end
 
+@views function clatrd!(A::AbstractMatrix,E::AbstractVector,W::AbstractMatrix,V::AbstractVector,tau::AbstractVector,n::Number,nb::Number)
+
+    @inbounds(for i=1:nb
+        #update A[i:n,i]
+
+        if i>1
+            mul!(A[i:n,i],A[i:n,1:i-1],W[i,1:i-1],1,1)
+            mul!(A[i:n,i],W[i:n,1:i-1],A[i,1:i-1],-1,1)
+        end
+
+        #Generate elementary reflector H(i) to annihilate A(i+2:n,i)
+
+        v,stau = householder_reflector!(A[i+1:n,i],V[i:n-1],n-i)
+        A[i+1,i] -= stau*dot(v,A[i+1:n,i])
+        E[i]   = A[i+1,i]
+        A[i+1:end,i] = v
+
+        mul!(W[i+1:n,i],A[i+1:n,i+1:n], A[i+1:n,i])  #Key point 60% of running time of sktrd!
+        #skmv!(A[i+1:n,i+1:n], A[i+1:n,i],W[i+1:n,i],n-i)
+        if i>1
+            mul!(W[1:i-1,i],W[i+1:n,1:i-1]',A[i+1:n,i])
+            mul!(W[i+1:n,i],A[i+1:n,1:i-1],W[1:i-1,i],1,1)
+            mul!(W[1:i-1,i],A[i+1:n,1:i-1]',A[i+1:n,i])
+            mul!(W[i+1:n,i],W[i+1:n,1:i-1],W[1:i-1,i],-1,1)
+        end
+        W[i+1:n,i] .*= stau
+
+        alpha = -stau*dot(W[i+1:n,i],A[i+1:n,i])/2
+        W[i+1:n,i].+=alpha.*A[i+1:n,i]
+        tau[i] = stau
+
+
+    end)
+    return
+end
+function set_nb(n::Integer)
+    if n<=12
+        return max(n-4,1)
+    elseif n<=100
+        return 10
+    else
+        return 60
+    end
+    return 1
+end
+
+@views function csktrd!(S::SkewHermitian{<:Real})
+    n = size(S.data,1)
+
+    if n == 1
+        return Hessenberg(Matrix(S.data),Vector{eltype(S.data)}(undef,0),LA.UpperHessenberg(S.data),'L')
+    end
+
+    nb  = set_nb(n)
+    A   = S.data
+
+    E   = similar(A,n-1)
+    tau = similar(A,n-2)
+    W   = similar(A, n, nb)
+    update = similar(A, n-nb, n-nb)
+    V   = similar(A, n-1)
+
+    oldi = 0
+
+    @inbounds(for i = 1:nb:n-nb-2
+        size = n-i+1
+
+        clatrd!(A[i:n,i:n],E[i:i+nb-1],W,V,tau[i:i+nb-1],size,nb)
+        mul!(update[1:n-nb-i+1,1:n-nb-i+1],A[i+nb:n,i:i+nb-1],W[nb+1:size,:]')
+
+        s = i+nb-1
+
+        for k = 1:n-s
+            A[s+k,s+k] = 0
+            @simd for j = k+1:n-s
+                A[s+j,s+k] += update[j,k]-update[k,j]'
+                A[s+k,s+j] = - A[s+j,s+k]
+            end
+
+        end
+        oldi = i
+    end)
+    oldi += nb
+    if oldi < n
+        skewhess!(A[oldi:n,oldi:n],tau[oldi:end],E[oldi:end])
+    end
+    E[end] = A[end,end-1]
+
+    return tau, E
+
+end
+
