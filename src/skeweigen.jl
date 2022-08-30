da6109689c1faf10cbfcb842c7aa329b8860994f
smataigne <simonmataigne@gmail.com>
2022-08-30 21:02:10 +0200

    Old criterion reintegrated

--- src/skeweigen.jl
+++ src/skeweigen.jl
@@ -171,8 +171,8 @@ end
         Ginit = similar(A, T, n)
         reducetozero(ev, Ginit, n)
     end
-    lim = (T<: Float32 ? 2f-7 : 3e-16)
-    tol = lim * norm(ev)
+    
+    tol = eps(T) * norm(ev)
     max_iter = 100 * n
     iter = 0 ;
     halfN = div(n,2)
