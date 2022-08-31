e4b22c861687ba9488447fe600b119433f6c07c2
Steven G. Johnson <stevenj@alum.mit.edu>
2022-08-31 15:35:46 -0400

    documentation index

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,19 @@ using .DocMeta: setdocmeta!
 setdocmeta!(SkewLinearAlgebra, :DocTestSetup, :(using SkewLinearAlgebra, LinearAlgebra);
             recursive=true)
 
-makedocs(modules=[SkewLinearAlgebra], sitename="SkewLinearAlgebra Documentation")
+makedocs(
+    modules = [SkewLinearAlgebra],
+    clean = false,
+    sitename = "SkewLinearAlgebra Documentation",
+    authors = "Simon Mataigne, Steven G. Johnson, and contributors.",
+    pages = [
+        "Overview" => "index.md",
+        "Matrix Types" => "types.md",
+        "Eigenproblems" => "eigen.md",
+        "Exponential/Trigonometric functions" => "trig.md",
+        "Pfaffians" => "pfaffian.md",
+        "Skew-Cholesky" => "skewchol.md",
+    ],
+)
 
 deploydocs(repo="github.com/JuliaLinearAlgebra/SkewLinearAlgebra.jl")
