8bc8dacb8ad50588dbd7f9b73909332a1c226953
Gertian <Gertian.Roose@Ugent.be>
2024-09-25 14:23:05 +0300

    Moved tests to test

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -20,14 +20,3 @@ function ChainRules.rrule(::typeof(pfaffian), x::Union{SkewHermitian})  #pfaffia
 end
 
 #perhaps should add more options here ? Defenitely we will need + , - , * . At this point I'm happy though If I can just take the derivative of Pfaffian and it's constructor. All other operators I can do with dense matrices for now...
-
-#we make a small test for this 
-A = skewhermitian(rand(2,2))
-B = skewhermitian(rand(2,2))
-
-test_rrule(pfaffian, A)   #this one doesn't work since it seems to be getting non skew symmetric matrices at some point
-
-test_rrule(pfaffian, A⊢B)  #SO I wanted to specify the tangent but the same happens, judging from our short conversation this might be because I'm ill defining the tangent B ?
-
-test_rrule(SkewHermitian, A.data ⊢ B.data)#constructor false because ΔFoo supposedly has no field data. But ΔFoo should be a SkewHermitian object, so it should have a field data.
-
