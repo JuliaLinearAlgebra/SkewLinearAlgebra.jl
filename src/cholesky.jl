dc19ea65c1084ee69ddc14f5552619381348e662
Simeon David Schaub <simeon@schaub.rocks>
2026-02-02 20:01:42 +0100

    address review comments

--- src/cholesky.jl
+++ src/cholesky.jl
@@ -18,38 +18,18 @@ Construct a `SkewCholesky` structure from the `UpperTriangular`
 matrix `R` and the permutation vector `p`. A matrix `J` of type `JMatrix`
 is build calling this function.
 The `SkewCholesky` structure has three arguments: `R`,`J` and `p`.
+`p` may be `Base.OneTo(n)`, which indicates no pivoting was used.
 """
 function SkewCholesky(R::UpperTriangular{<:T},p::AbstractVector{<:Integer}) where {T<:Real}
     n = size(R, 1)
     return SkewCholesky{T,typeof(R),JMatrix{T,+1},typeof(p)}(R, JMatrix{T,+1}(n), p)
 end
 
-struct SkewCholeskyNoPivot{T,R<:UpperTriangular{<:T},J<:JMatrix{<:T}}
-    R::R #Uppertriangular matrix
-    J::J # Block diagonal skew-symmetric matrix of type JMatrix
-
-    function SkewCholeskyNoPivot{T,R,J}(Rm,Jm) where {T,R,J}
-        LA.require_one_based_indexing(Rm)
-        new{T,R,J}(Rm,Jm)
-    end
-end
-
-"""
-    SkewCholeskyNoPivot(R)
-
-Construct a `SkewCholeskyNoPivot` structure from the `UpperTriangular`
-matrix `R`. A matrix `J` of type `JMatrix` is build calling this function.
-The `SkewCholeskyNoPivot` structure has two arguments: `R` and `J`.
-"""
-function SkewCholeskyNoPivot(R::UpperTriangular{<:T}) where {T<:Real}
-    n = size(R, 1)
-    return SkewCholeskyNoPivot{T,typeof(R),JMatrix{T,+1}}(R, JMatrix{T,+1}(n))
-end
-
-function _skewchol_nopivot!(A::SkewHermitian{<:Real})
+function _skewchol!(A::SkewHermitian{<:Real}, ::NoPivot)
     B = A.data
     m = size(B,1)
-    m == 1 && return
+    P = Base.OneTo(m)
+    m == 1 && return P
     J2 = similar(B,2,2)
     J2[1,1] = 0; J2[2,1] = -1; J2[1,2] = 1; J2[2,2] = 0
     tempM = similar(B,2,m-2)
@@ -72,10 +52,10 @@ function _skewchol_nopivot!(A::SkewHermitian{<:Real})
         @views mul!(tempM[:,1:l], J2, B[j2-1:j2,j2+1:m])
         @views mul!(B[j2+1:m,j2+1:m], transpose(B[j2-1:j2,j2+1:m]), tempM[:,1:l],-1,1)
     end
-    return
+    return P
 end
 
-function _skewchol!(A::SkewHermitian{<:Real})
+function _skewchol!(A::SkewHermitian{<:Real}, ::RowMaximum)
     B = A.data
     tol = 1e-15 * norm(B)
     m = size(B,1)
@@ -131,19 +111,12 @@ function _skewchol!(A::SkewHermitian{<:Real})
 end
 copyeigtype(A::AbstractMatrix) = copyto!(similar(A, LA.eigtype(eltype(A))), A)
 
-@views function skewchol!(A::SkewHermitian, ::LA.RowMaximum)
-    P = _skewchol!(A)
+@views function skewchol!(A::SkewHermitian, pivot::Union{RowMaximum,NoPivot}=RowMaximum())
+    P = _skewchol!(A, pivot)
     return SkewCholesky(UpperTriangular(A.data), P)
 end
 
-@views function skewchol!(A::SkewHermitian, ::LA.NoPivot)
-    _skewchol_nopivot!(A)
-    return SkewCholeskyNoPivot(UpperTriangular(A.data))
-end
-
-skewchol!(A::SkewHermitian) = skewchol!(A, LA.RowMaximum())
-
-skewchol(A::SkewHermitian, pivot::Union{LA.RowMaximum,LA.NoPivot}=LA.RowMaximum()) =
+skewchol(A::SkewHermitian, pivot::Union{RowMaximum,NoPivot}=RowMaximum()) =
     skewchol!(copyeigtype(A), pivot)
 
 """
@@ -151,26 +124,23 @@ skewchol!(A, [pivot])
 
 Similar to [`skewchol`](@ref), but overwrites `A` in-place with intermediate calculations.
 """
-skewchol!(A::AbstractMatrix, pivot::Union{LA.RowMaximum,LA.NoPivot}=LA.RowMaximum()) =
+skewchol!(A::AbstractMatrix, pivot::Union{RowMaximum,NoPivot}=RowMaximum()) =
     @views skewchol!(SkewHermitian(A), pivot)
 
 """
     skewchol(A, [pivot::Union{RowMaximum,NoPivot}])
 
 Computes a Cholesky-like factorization of the real skew-symmetric matrix `A`.
-The function returns a `SkewCholesky` or `SkewCholeskyNoPivot` structure
-depending on whether pivoting is used.
+The function returns a `SkewCholesky` factorization object.
 
 With `RowMaximum()` (default), the result is a `SkewCholesky` with fields `R`, `J`, `p`,
-such that `S.R'*S.J*S.R = A[S.p,S.p]`.
-
-With `NoPivot()`, the result is a `SkewCholeskyNoPivot` with fields `R`, `J`,
-such that `S.R'*S.J*S.R = A`.
+such that `S.R'*S.J*S.R = A[S.p,S.p]`. Pivoting can be disabled by passing `NoPivot()`
+as the second argument, in which case `S.p` is `Base.OneTo(n)`, such that `S.R'*S.J*S.R = A`.
 
 This factorization (and the underlying algorithm) is described in from P. Benner et al,
 "[Cholesky-like factorizations of skew-symmetric matrices](https://etna.ricam.oeaw.ac.at/vol.11.2000/pp85-93.dir/pp85-93.pdf)"(2000).
 """
-function skewchol(A::AbstractMatrix, pivot::Union{LA.RowMaximum,LA.NoPivot}=LA.RowMaximum())
+function skewchol(A::AbstractMatrix, pivot::Union{RowMaximum,NoPivot}=RowMaximum())
     isskewhermitian(A) || throw(ArgumentError("Pfaffian requires a skew-Hermitian matrix"))
     return skewchol!(SkewHermitian(copyeigtype(A)), pivot)
 end
